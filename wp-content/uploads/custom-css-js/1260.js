f885cd21bc2de5b1ce21c3a352a0be328b8bece5
sennestans <senne.stans@gmail.com>
2025-09-06 15:43:05 +0200

    first push

---
+++ wp-content/uploads/custom-css-js/1260.js
@@ -0,0 +1,19 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+document.addEventListener("DOMContentLoaded", function() {
+  const targets = document.querySelectorAll('.fluor-text');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('inview');
+        // Wil je dat het telkens opnieuw gebeurt? haal dan de volgende regel weg
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 }); // 20% zichtbaar is genoeg
+
+  targets.forEach(el => observer.observe(el));
+});
+</script>
+<!-- end Simple Custom CSS and JS -->
---
+++ wp-content/uploads/custom-css-js/1534.js
@@ -0,0 +1,17 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+document.addEventListener('click', function(e){
+  const btn = e.target.closest('.menu-toggle, .ast-mobile-menu-trigger, .ast-header-burger, .ast-builder-menu-toggle');
+  if(!btn) return;
+  setTimeout(function(){
+    const expanded = btn.getAttribute('aria-expanded') === 'true';
+    document.body.classList.toggle('has-shrink', expanded);
+  }, 0);
+});
+
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
